efcae019b384ab4f587a9d034db8b23ae8496943
Weisaw <meysamm74@gmail.com>
2022-07-07 01:15:43 +0430

    improved gui

--- calculator_v2.py
+++ calculator_v2.py
@@ -62,15 +62,15 @@ btn_6 = Button(root, text='6', padx=40, pady=20, fg='white', bg='gray',  command
 btn_7 = Button(root, text='7', padx=40, pady=20, fg='white', bg='gray',  command=lambda: calc_click(7))
 btn_8 = Button(root, text='8', padx=40, pady=20, fg='white', bg='gray',  command=lambda: calc_click(8))
 btn_9 = Button(root, text='9', padx=40, pady=20, fg='white', bg='gray',  command=lambda: calc_click(9))
-btn_0 = Button(root, text='0', padx=40, pady=20, fg='white', bg='gray',  command=lambda: calc_click(0))
-btn_dot = Button(root, text='.', padx=42, pady=20, fg='white', bg='gray',  command=lambda: calc_click('.'))
+btn_0 = Button(root, text='0', padx=88, pady=20, fg='white', bg='gray',  command=lambda: calc_click(0))
+btn_dot = Button(root, text='.', padx=41, pady=20, fg='white', bg='gray',  command=lambda: calc_click('.'))
 
-btn_add_sign = Button(root, text='+', padx=45, pady=52, fg='black', bg='lightblue', command=lambda: calc_click('+'))
+btn_add_sign = Button(root, text='+', padx=45, pady=20, fg='black', bg='lightblue', command=lambda: calc_click('+'))
 btn_subtract_sign = Button(root, text='─', padx=42, pady=20, fg='black', bg='lightblue', command=lambda: calc_click('-'))
 btn_multiply_sign = Button(root, text='*', padx=41, pady=10, fg='black', bg='lightblue', command=lambda: calc_click('*'))
 btn_devide_sign = Button(root, text='/', padx=41, pady=10, fg='black', bg='lightblue', command=lambda: calc_click('/'))
 
-btn_equal_sign = Button(root, text='=', padx=91, pady=20, bg='lightgreen', command=calc)
+btn_equal_sign = Button(root, text='=', padx=44, pady=52, bg='lightgreen', command=calc)
 
 btn_clear = Button(root, text='Clear', padx=30, pady=10, fg='black', bg='lightblue', command=calc_clear)
 btn_del = Button(root, text='del', padx=40, pady=10, fg='black', bg='lightblue', command=calc_del)
@@ -88,16 +88,17 @@ btn_7.grid(row=2,column=0)
 btn_8.grid(row=2,column=1)
 btn_9.grid(row=2,column=2)
 
-btn_0.grid(row=5,column=0)
-btn_dot.grid(row=5, column=1)
-btn_equal_sign.grid(row=5, column=2, columnspan=2)
+btn_0.grid(row=5,column=0, columnspan=2)
+btn_dot.grid(row=5, column=2)
 
-btn_add_sign.grid(row=3, column=3, rowspan=2)
+btn_equal_sign.grid(row=4, column=3, rowspan=2)
+
+btn_add_sign.grid(row=3, column=3)
+
 btn_subtract_sign.grid(row=2, column=3)
 
 btn_multiply_sign.grid(row=1, column=0)
 btn_devide_sign.grid(row=1, column=1)
-
 btn_clear.grid(row=1, column=2)
 btn_del.grid(row=1, column=3)
 
